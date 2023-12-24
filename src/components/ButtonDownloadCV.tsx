303d2f41fbd7e93160360ed98784ff75e4a8f1d4
JhonanCaleb <jhonancalebm@gmail.com>
2023-12-24 16:42:45 -0500

    some fixes

--- src/components/ButtonDownloadCV.tsx
+++ src/components/ButtonDownloadCV.tsx
@@ -1,7 +1,7 @@
 function ButtonDownloadCV() {
   return (
     <a
-      href="https://jhonanmc.com/assets/my_cv-19963eed.pdf"
+      href="https://jhonanmc.com/assets/cv_jhonanmc-eeb1b3bf.pdf"
       target="_blank"
       className="fixed top-5 right-5 px-3 py-2 bg-primary text-white rounded-md font-medium hover:bg-primary/80"
     > 
